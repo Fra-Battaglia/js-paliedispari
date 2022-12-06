b1a06fcc19628c4b620562d409cc5c55e6aca964
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-12-06 09:51:23 +0100

    player number done

--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -13,3 +13,19 @@ while (index < 5){
 		prompt('inserisci pari o dispari')
 	}
 }
+
+//2 - permettere con un prompt l'inserimeto di un numero da 1 a 5
+let number = parseInt(prompt('inserisci un numero da 1 a 5'));
+
+let i = 0;
+
+while (i < 2) {
+	if (number >= 1 && number<= 5) {
+		console.log(number);
+		i = 2;
+	}
+	else {
+		alert('inserisci un valore valido');
+		prompt('inserisci un numero da 1 a 5');
+	}
+}
