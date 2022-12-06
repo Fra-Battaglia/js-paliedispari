f690253d083df01623780509b5464410fbeedf3c
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-12-06 09:53:33 +0100

    odd even check done, exercise done

--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -41,3 +41,16 @@ console.log(computer_number);
 //4 - somma i 2 numeri
 let sum = number + computer_number;
 console.log(sum);
+
+//5 - SE l'utente ha scelto pari o dispari e la somma dei numeri corrisponde alla scelta mostrare il messaggio 'Hai vinto'
+if (odd_even == 'pari' && sum % 2 == 0) {
+	console.log('hai vinto');
+}
+else if (odd_even == 'dispari' && sum % 2 != 0) {
+	console.log('hai vinto');
+}
+
+//5.1 - ALTRIMENTI mostrare il messaggio 'Hai perso'
+else {
+	console.log('hai perso');
+}
